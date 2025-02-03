3083e5ee0e7c77b7f47a57120daadeb6d7b7f95c
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2025-02-04 00:36:00 +0530

    fixed bug

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='bg-red-500 text-4xl'>Hello world</h1>
+      <Button >Clcik Me</Button>
+    </div>
+  )
+}
+
+export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
+    darkMode: ['class'],
     content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
   	extend: {
